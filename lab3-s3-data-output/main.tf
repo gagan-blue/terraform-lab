1319c900cb104a2f7d114ba7a2c358eed61bcb03
you <you@example.com>
2019-08-04 19:28:37 +0530

    c

--- lab3-s3-data-output/main.tf
+++ lab3-s3-data-output/main.tf
@@ -4,7 +4,6 @@ provider "aws" {
 
 resource "aws_s3_bucket" "s3lab" {
   bucket = "mybucket15apirl1986"
-  #acl    = "public-write"
   region = "ap-south-1"
 
 }
@@ -13,14 +12,14 @@ resource "aws_s3_bucket_object" "uploadfile" {
   bucket = "${aws_s3_bucket.s3lab.id}"
   source = "test.txt"
   key    = "tf/mytest.txt"
-  //content = "application/json"
-  //content= "binary/octet-stream"
+  #content = "application/json"
+  #content= "binary/octet-stream"
   content_type="text/plain" #valid MIME Types
 }
 
 #after adding data / output run terraform refresh
 data "aws_s3_bucket_object" "mys3object" {
-  bucket = "${aws_s3_bucket.s3lab.id}"
+  bucket = "${aws_s3_bucket_object.uploadfile.bucket}"
   key    = "${aws_s3_bucket_object.uploadfile.key}"
 }
 
@@ -29,10 +28,12 @@ output "s3file" {
 }
 
 
+
 #terraform state list
 
     #aws_s3_bucket.s3lab
     #aws_s3_bucket_object.uploadfile
+    #data.aws_s3_bucket_object.mys3object
 
 #stdout of terraform apply
 
@@ -41,7 +42,26 @@ output "s3file" {
     #data.aws_s3_bucket_object.mys3object: Refreshing state...
 
     #Apply complete! Resources: 1 added, 0 changed, 0 #destroyed.
-
     #Outputs:
-
     #s3file = to be upload to s3
+
+
+
+# terraform state show data.aws_s3_bucket_object.mys3object
+
+#     body           = "to be upload to s3\n\n"
+#     bucket         = "mybucket15apirl1986"
+#     content_length = 20
+#     content_type   = "text/plain"
+#     etag           = "b46a5d880a2b68712de4ec91e53377ca"
+#     id             = "mybucket15apirl1986/tf/mytest.txt"
+#     key            = "tf/mytest.txt"
+#     last_modified  = "Sun, 04 Aug 2019 12:27:09 UTC"
+#     metadata       = {}
+#     storage_class  = "STANDARD"
+#     tags           = {}
+
+# less terraform.tfstate
+
+
+
