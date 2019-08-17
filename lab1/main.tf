bf9773f3edcbe09d8c10a4c573fc55e9f9a51b84
gagan <gagan.blue@gmail.com>
2019-08-17 17:38:19 +0530

    Update main.tf

--- lab1/main.tf
+++ lab1/main.tf
@@ -14,6 +14,14 @@ output "public-dns" {
  value = "${aws_instance.apache.*.public_dns}"
 }
 
+#terraform output public-dns
+    #[
+    #  "ec2-13-232-177-218.ap-south-1.compute.amazonaws.com",
+    #   "ec2-13-233-168-159.ap-south-1.compute.amazonaws.com",
+    #]
+
+
+
 #echo "aws_instance.apache.0.public_dns" | terraform console
     #ec2-13-232-177-218.ap-south-1.compute.amazonaws.com
 #gagan-Tower> echo "aws_instance.apache.1.public_dns" | terraform console
